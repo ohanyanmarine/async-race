e5c5a2a70733d07e4a69840591eacd95fb24a6d1
ohanyanmarine <m.v.ohanyan@gmail.com>
2025-09-15 15:00:32 +0400

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    "react/require-default-props": "off",
+    "react/prop-types": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'import/extensions': 'off',
+    'no-plusplus': 'off',
+    "no-console": "off",
+    "no-alert": "off",
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
